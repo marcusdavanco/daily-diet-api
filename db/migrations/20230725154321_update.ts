cd0d2a1c544fe8d4f60346cb373aa0bb6a320bc9
Marcus Davanço <md.daves@gmail.com>
2023-07-25 12:54:29 -0300

    fix: migration timestamps

---
+++ db/migrations/20230725154321_update.ts
@@ -0,0 +1,18 @@
+import { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('meals', (table) => {
+    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
+    table.timestamp('updated_at').defaultTo(knex.fn.now()).notNullable()
+  })
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('meals', (table) => {
+    table.dropColumn('created_at')
+    table.dropColumn('updated_at')
+  })
+}
+
