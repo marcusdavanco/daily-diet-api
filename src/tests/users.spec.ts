18cddbd1f8b01e4d29e8703d8656deaaf71e879c
Marcus Davanço <md.daves@gmail.com>
2023-07-31 04:01:42 -0300

    chore: user view route

--- src/tests/users.spec.ts
+++ src/tests/users.spec.ts
@@ -1,4 +1,4 @@
-import { it, afterAll, beforeAll, describe } from 'vitest'
+import { it, afterAll, beforeAll, describe, expect } from 'vitest'
 import request from 'supertest'
 import { app } from '../app'
 
@@ -11,12 +11,35 @@ describe('User routes', () => {
     await app.close()
   })
 
-  it('should be able to create a new account', async () => {
+  it.skip('should be able to view a user', async () => {
+    const createAccountResponse = await request(app.server)
+      .post('/users')
+      .send({
+        name: 'mu Doe',
+        email: 'mu@email.com'
+      })
+
+     const [cookies] = createAccountResponse.get('Set-Cookie')
+
+     const listUserResponse = await request(app.server)
+      .get(`/users/${cookies}`)
+      .set('Cookie', cookies)
+      .expect(200)
+
+      expect(listUserResponse.body.users).toEqual([
+        expect.objectContaining({
+          name: 'Jeff Doe',
+          email: 'jeffdoe@email.com'
+        })
+      ])
+  })
+
+  it.skip('should be able to create a new account', async () => {
     const response = await request(app.server)
       .post('/users')
       .send({
-        name: 'John Foe',
-        email: 'johnfoe@email.com',
+        name: 'John John',
+        email: 'johnjohn@email.com',
       })
       .expect(201)
   })
