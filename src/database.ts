6d9c449a615b9ffa17bbcd4daec11c1ac2db0c1e
Marcus Davanço <md.daves@gmail.com>
2023-07-31 12:07:58 -0300

    feat: preparing for production

--- src/database.ts
+++ src/database.ts
@@ -7,9 +7,9 @@ if (!process.env.DATABASE_URL) {
 
 export const config: Knex.Config = {
   client: 'sqlite',
-  connection: {
+  connection: env.DATABASE_CLIENT === 'sqlite' ? {
     filename: env.DATABASE_URL,
-  },
+  }: env.DATABASE_URL,
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
